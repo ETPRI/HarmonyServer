98565c4ba1cd57d636da6945db339aff77fa5656
AmyFiori <fiori_amanda@yahoo.com>
2018-07-09 13:14:46 -0400

    Minor changes
    Updated gitignore to ignore .DS_Store, and deleted references to neo4j.

--- servedFiles/Code/app.js
+++ servedFiles/Code/app.js
@@ -12,6 +12,8 @@ constructor() {
 	this.idCounter = 0;  // init id counter - used get getElementById, is the id of the widget
 
 	// used by class DB to access neo4j database
+	// this.authToken = neo4j.v1.auth.basic("neo4j", "paleo3i");
+	// this.driver    = neo4j.v1.driver("bolt://localhost", this.authToken, {encrypted:false});
 
 	this.activeWidget = null; // widget being dragged
 }
