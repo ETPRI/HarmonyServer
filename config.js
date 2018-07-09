be8fd98861da4c50dd6dc246d3d048ca5311ab5c
David Bolt <dwbolt@etpri.org>
2018-07-08 22:02:04 -0400

    cosmos-db support

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.endpoint = "murduk.gremlin.cosmosdb.azure.com";
+config.primaryKey = "BSGbfbwdycuuB7R79lyz6UA35bBJ9SW7hy1AT1Mrt5my92BfU5nsXw80GoQwF7EDX5pt53QViX1KoldXr9OIaQ==";
+config.database = "graphdb"
+config.collection = "Persons"
+
+module.exports = config;
